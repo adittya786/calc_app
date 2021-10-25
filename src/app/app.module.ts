3d766043311de2ebf1fc0039ae54164ff39db359
unknown <adittya_201900262@smit.smu.edu.in>
2021-10-25 21:26:19 +0530

    My calculator app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
-
+import { CalculatorComponent } from './calculator/calculator.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [AppComponent, CalculatorComponent],
+imports: [
+BrowserModule,
+MatCardModule,
+NoopAnimationsModule,
+MatButtonModule,
+MatIconModule,
+MatToolbarModule,
+],
+providers: [],
+bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
